c8d99f1fac4ae3659a2c867e8543295924535876
diamant <alexandre.diamant@etu.u-paris.fr>
2023-01-10 13:28:58 +0100

    I-2 autre mode de jeu

--- src/XpatSolver.ml
+++ src/XpatSolver.ml
@@ -69,107 +69,155 @@ let rec length_array array =
   | [| |] -> 0
   | _ -> 1 + length_array (Array.sub array 1 (Array.length array - 1))
 
-let move_to_col_freecell (card_num : int) (dest_num : int) n = 
-  (* Si les cartes card et dest ne sont pas de couleurs différentes ou/et que le numero
-     de card n'est pas egal au numero de dest-1, c'est un ECHEC*)
-     let card_of_card_num = Card.of_num card_num in
-     let card_of_dest_num = Card.of_num dest_num in
-   
-     let color card = 
-     match card with
-     | (_, c) -> match c with 
-                 | Card.Trefle | Card.Pique -> "black"
-                 | Card.Carreau | Card.Coeur -> "red" in
-   
-     let num card =
-     match card with
-     | (n, _) -> n in
-   
-     let card_color = color card_of_card_num in
-     let dest_color = color card_of_dest_num in
-   
-     if card_color = dest_color || num card_of_card_num <> num card_of_dest_num - 1 then "ECHEC "^string_of_int n
-     else
-       (* On cherche la colonne où se trouve la carte card. Elle doit être la dernière carte de sa colonne *)
-       let colonnes_length = length_array !colonnes in
-       let rec loop1 n = 
-         match n with
-         | n -> 
-             if n = colonnes_length then colonnes_length
-             else
-               let col_length = length_array !colonnes.(n) in
-               let pos = ref (col_length - 1) in
-               if !colonnes.(n).(!pos) = card_of_card_num then n
-               else loop1 (n+1)
-       in
-       let col_card = loop1 0 in
-   
-       (* On cherche la colonne où se trouve la carte dest. Elle doit être la dernière carte de sa colonne *)
-       let rec loop2 n = 
-         match n with
-         | n -> 
-             if n = colonnes_length then colonnes_length
-             else
-               let col_length = length_array !colonnes.(n) in
-               let pos = ref (col_length - 1) in
-               if !colonnes.(n).(!pos) = card_of_dest_num then n
-               else loop2 (n+1)
-       in
-       let col_dest = loop2 0 in
-    
-       (* Si col_dest = colonnes_length -> ECHEC *)
-   
-       if col_dest = colonnes_length || col_card = colonnes_length then "ECHEC "^string_of_int n
-       else
-         (* On deplace la carte card sur la carte dest *)
-         (* On retire la carte card de sa colonne *)
-         let col_card_length = length_array !colonnes.(col_card) in
-         let pos_card = ref (col_card_length - 1) in
-         let card = !colonnes.(col_card).(!pos_card) in
-         let new_col_card = Array.sub !colonnes.(col_card) 0 (!pos_card) in
-         Array.set !colonnes col_card new_col_card;
-   
-         (* On ajoute la carte card à la colonne de dest *)
-         let col_dest_length = length_array !colonnes.(col_dest) in
-         let pos_dest = ref (col_dest_length - 1) in
-         let new_col_dest = Array.make (col_dest_length + 1) card in
-         Array.blit !colonnes.(col_dest) 0 new_col_dest 0 (col_dest_length);
-         Array.set !colonnes col_dest new_col_dest;
-         "SUCCES";;
+let color card =
+  match card with
+  | (_, c) -> match c with
+              | Card.Trefle | Card.Pique -> "black"
+              | Card.Carreau | Card.Coeur -> "red"
+let num card =
+  match card with
+  | (n, _) -> n
 
-let move_to_col_seahaven (card_num : int) (dest_num : int) n = "TODO";;
+let suit card =
+  match card with 
+  | (_, s) -> match s with
+              | Card.Trefle -> "Trefle"
+              | Card.Carreau -> "Carreau"
+              | Card.Coeur -> "Coeur"
+              | Card.Pique -> "Pique"
 
-let move_to_col_midnight (card_num : int) (dest_num : int) n = "TODO";;
+let is_same_color (card1 : Card.card) (card2 : Card.card) =
+  let color1 = color card1 in
+  let color2 = color card2 in
+  color1 = color2
 
-let move_to_col_baker (card_num : int) (dest_num : int) n = "TODO";;
+let is_same_suit (card1 : Card.card) (card2 : Card.card) =
+  let suit1 = suit card1 in
+  let suit2 = suit card2 in
+  suit1 = suit2
 
-let move_to_empty_col_freecell (card_num : int) n = 
-  let card_of_card_num = Card.of_num card_num in
-  (* On cherche la colonne où se trouve la carte card. Elle doit être la dernière carte de sa colonne *)
-  let colonnes_length = length_array !colonnes in
+let find_col array card =
+  let colonnes_length = length_array !array in
   let rec loop1 n = 
     match n with
     | n -> 
         if n = colonnes_length then colonnes_length
         else
-          let col_length = length_array !colonnes.(n) in
+          let col_length = length_array !array.(n) in
           let pos = ref (col_length - 1) in
-          if !colonnes.(n).(!pos) = card_of_card_num then n
+          if !colonnes.(n).(!pos) = card then n
           else loop1 (n+1)
-  in
-  let col_card = loop1 0 in
+    in
+    loop1 0 
 
-  (* On trouve la premiere colonne vide *)
-  let rec loop2 n = 
+let move_card col_card col_dest =
+  (* On deplace la carte card sur la carte dest *)
+  (* On retire la carte card de sa colonne *)
+  let col_card_length = length_array !colonnes.(col_card) in
+  let pos_card = ref (col_card_length - 1) in
+  let card = !colonnes.(col_card).(!pos_card) in
+  let new_col_card = Array.sub !colonnes.(col_card) 0 (!pos_card) in
+  Array.set !colonnes col_card new_col_card;
+  (* On ajoute la carte card à la colonne de dest *)
+  let col_dest_length = length_array !colonnes.(col_dest) in
+  let pos_dest = ref (col_dest_length - 1) in
+  let new_col_dest = Array.make (col_dest_length + 1) card in
+  Array.blit !colonnes.(col_dest) 0 new_col_dest 0 (col_dest_length);
+  Array.set !colonnes col_dest new_col_dest;
+  "SUCCES";;
+
+let move_to_col_freecell (card_num : int) (dest_num : int) n = 
+  (* Si les cartes card et dest ne sont pas de couleurs différentes ou/et que le numero
+     de card n'est pas egal au numero de dest-1, c'est un ECHEC*)
+  let card_of_card_num = Card.of_num card_num in
+  let card_of_dest_num = Card.of_num dest_num in
+
+  if is_same_color card_of_card_num card_of_dest_num || num card_of_card_num <> num card_of_dest_num - 1 then "ECHEC "^string_of_int n
+  else
+    (* On cherche la colonne où se trouve la carte card. Elle doit être la dernière carte de sa colonne *)
+    let col_card = find_col colonnes card_of_card_num in
+
+    (* On cherche la colonne où se trouve la carte dest. Elle doit être la dernière carte de sa colonne *)
+    let col_dest = find_col colonnes card_of_dest_num in
+
+    (* Si col_dest = colonnes_length -> ECHEC *)
+    let colonnes_length = length_array !colonnes in
+    if col_dest = colonnes_length || col_card = colonnes_length then "ECHEC "^string_of_int n
+    else
+      move_card col_card col_dest;;
+
+let move_to_col_seahaven (card_num : int) (dest_num : int) n = 
+  let card_of_card_num = Card.of_num card_num in
+  let card_of_dest_num = Card.of_num dest_num in
+
+  if is_same_suit card_of_card_num card_of_dest_num = false || num card_of_card_num <> num card_of_dest_num - 1 then "ECHEC "^string_of_int n
+  else
+    (* On cherche la colonne où se trouve la carte card. Elle doit être la dernière carte de sa colonne *)
+    let col_card = find_col colonnes card_of_card_num in
+   
+    (* On cherche la colonne où se trouve la carte dest. Elle doit être la dernière carte de sa colonne *)
+    let col_dest = find_col colonnes card_of_dest_num in
+
+    let colonnes_length = length_array !colonnes in
+    if col_dest = colonnes_length || col_card = colonnes_length then "ECHEC "^string_of_int n
+    else 
+      move_card col_card col_dest;;
+
+let move_to_col_midnight (card_num : int) (dest_num : int) n = 
+  let card_of_card_num = Card.of_num card_num in
+  let card_of_dest_num = Card.of_num dest_num in
+
+  if is_same_suit card_of_card_num card_of_dest_num = false || num card_of_card_num <> num card_of_dest_num - 1 then "ECHEC "^string_of_int n
+  else 
+    (* On cherche la colonne où se trouve la carte card. Elle doit être la dernière carte de sa colonne *)
+    let col_card = find_col colonnes card_of_card_num in
+   
+    (* On cherche la colonne où se trouve la carte dest. Elle doit être la dernière carte de sa colonne *)
+    let col_dest = find_col colonnes card_of_dest_num in
+
+    let colonnes_length = length_array !colonnes in
+    if col_dest = colonnes_length || col_card = colonnes_length then "ECHEC "^string_of_int n
+    else 
+      move_card col_card col_dest;;
+
+let move_to_col_baker (card_num : int) (dest_num : int) n = 
+  let card_of_card_num = Card.of_num card_num in
+  let card_of_dest_num = Card.of_num dest_num in
+
+  if num card_of_card_num <> num card_of_dest_num - 1 then "ECHEC "^string_of_int n
+  else 
+    (* On cherche la colonne où se trouve la carte card. Elle doit être la dernière carte de sa colonne *)
+    let col_card = find_col colonnes card_of_card_num in
+   
+    (* On cherche la colonne où se trouve la carte dest. Elle doit être la dernière carte de sa colonne *)
+    let col_dest = find_col colonnes card_of_dest_num in
+
+    let colonnes_length = length_array !colonnes in
+    if col_dest = colonnes_length || col_card = colonnes_length then "ECHEC "^string_of_int n
+    else 
+      move_card col_card col_dest;;
+
+let find_empty_col colonnes =
+  let colonnes_length = length_array !colonnes in
+  let rec loop n = 
     match n with
     | n -> 
         if n = colonnes_length then colonnes_length
         else
           let col_length = length_array !colonnes.(n) in
           if col_length = 0 then n
-          else loop2 (n+1)
+          else loop (n+1)
   in
-  let col_dest = loop2 0 in
+  loop 0;;
+
+let move_to_empty_col_freecell (card_num : int) n = 
+  let card_of_card_num = Card.of_num card_num in
+  (* On cherche la colonne où se trouve la carte card. Elle doit être la dernière carte de sa colonne *)
+  let col_card = find_col colonnes card_of_card_num in
+  (* On trouve la premiere colonne vide *)
+  let col_dest = find_empty_col colonnes in
+
+  let colonnes_length = length_array !colonnes in
   (* Si col_dest = colonnes_length || col_card = colonnes_length -> ECHEC *)
   if col_dest = colonnes_length || col_card = colonnes_length then "ECHEC "^string_of_int n
   else 
@@ -184,35 +232,55 @@ let move_to_empty_col_freecell (card_num : int) n =
     Array.set !colonnes col_dest new_col_dest;
     "SUCCES";;
 
-
-let move_to_registre_freecell (card_num : int) n = 
+let move_to_empty_col_seahaven (card_num : int) n =
   let card_of_card_num = Card.of_num card_num in
-  (* On cherche la colonne où se trouve la carte card. Elle doit être la dernière carte de sa colonne *)
-  let colonnes_length = length_array !colonnes in
-  let rec loop1 n = 
-    match n with
-    | n -> 
-        if n = colonnes_length then colonnes_length
-        else
-          let col_length = length_array !colonnes.(n) in
-          let pos = ref (col_length - 1) in
-          if !colonnes.(n).(!pos) = card_of_card_num then n
-          else loop1 (n+1)
-  in
-  let col_card = loop1 0 in
-  
-  (* On trouve un registre vide *)
+  (* On teste si la carte est un roi *)
+  let card_num = num card_of_card_num in
+  if card_num <> 13 then "ECHEC "^string_of_int n
+  else
+    (* On cherche la colonne où se trouve la carte card. Elle doit être la dernière carte de sa colonne *)
+    let col_card = find_col colonnes card_of_card_num in
+    (* On trouve la premiere colonne vide *)
+    let col_dest = find_empty_col colonnes in
+
+    let colonnes_length = length_array !colonnes in
+    (* Si col_dest = colonnes_length || col_card = colonnes_length -> ECHEC *)
+    if col_dest = colonnes_length || col_card = colonnes_length then "ECHEC "^string_of_int n
+    else 
+      (* On retire la carte card de sa colonne *)
+      let col_card_length = length_array !colonnes.(col_card) in
+      let pos_card = ref (col_card_length - 1) in
+      let card = !colonnes.(col_card).(!pos_card) in
+      let new_col_card = Array.sub !colonnes.(col_card) 0 (!pos_card) in
+      Array.set !colonnes col_card new_col_card;
+      (* Et la place dans la colonne vide *)
+      let new_col_dest = Array.make 1 card in
+      Array.set !colonnes col_dest new_col_dest;
+      "SUCCES";;
+
+let find_empty_registre registre =
   let registres_length = 4 in
-  let rec loop2 n = 
+  let rec loop n = 
     match n with
     | n -> 
         if n = registres_length then registres_length
         else
           let reg_length = length_array !registre.(n) in
           if reg_length = 0 then n
-          else loop2 (n+1)
-    in 
-  let reg_dest = loop2 0 in
+          else loop (n+1)
+  in
+  loop 0;;
+
+let move_to_registre_fc_st (card_num : int) n = 
+  let card_of_card_num = Card.of_num card_num in
+  (* On cherche la colonne où se trouve la carte card. Elle doit être la dernière carte de sa colonne *)
+  let col_card = find_col colonnes card_of_card_num in
+  
+  (* On trouve un registre vide *)
+  let reg_dest = find_empty_registre registre in
+
+  let colonnes_length = length_array !colonnes in
+  let registres_length = 4 in
   (* Si reg_dest = registres_length || col_card = registres_length -> ECHEC *)
   if reg_dest = registres_length || col_card = colonnes_length then "ECHEC "^string_of_int n
   else
@@ -238,13 +306,17 @@ let move_to_col (card_num : int) (dest_num : int) game (n : int) =
 let move_to_empty_col (card_num : int) game (n : int) = 
   match game with
   | Freecell -> move_to_empty_col_freecell card_num n
-  | _ -> "TODO";; 
+  | Seahaven -> move_to_empty_col_seahaven card_num n
+  | Midnight -> "Impossible de déplacer une carte dans une colonne vide dans Midnight:\n ECHEC "^string_of_int n
+  | Baker -> "Impossible de déplacer une carte dans une colonne vide dans Baker:\n ECHEC "^string_of_int n
 
 
 let move_to_registre (card_num : int) game (n : int) = 
   match game with
-  | Freecell -> move_to_registre_freecell card_num n
-  | _ -> "TODO";;
+  | Freecell -> move_to_registre_fc_st card_num n
+  | Seahaven -> move_to_registre_fc_st card_num n
+  | Midnight -> "Impossible de déplacer une carte dans un registre dans Midnight:\n ECHEC "^string_of_int n
+  | Baker -> "Impossible de déplacer une carte dans un registre dans Baker:\n ECHEC "^string_of_int n
   
 let move (card_num : int) (dest_num : int) game (n : int) = 
   match dest_num with
